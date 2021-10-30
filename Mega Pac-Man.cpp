21c9033a5c71ac2d4b94c33e0a5e6e4c8070ae33
SDarKnight <stepan.y.kravchenko@gmail.com>
2021-10-30 11:56:16 +0300

    Init project.

---
+++ Mega Pac-Man.cpp
@@ -0,0 +1,34 @@
+// Mega Pac-Man.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <windows.h>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+char screen[13][26] = {
+	"------------ ------------",
+	"|                       |",
+	"| OOOO   OOO OOO   OOOO |",
+	"|    O O  OO OO  O O    |",
+	"| OO   OO       OO   OO |",
+	"| OOOO OO OOOOO OO OOOO |",
+	"          O   O          ",
+	"| OOOO OO OOOOO OO OOOO |",
+	"| OO   OO       OO   OO |",
+	"|    O O  OO OO  O O    |",
+	"| OOOO   OOO OOO   OOOO |",
+	"|                       |",
+	"------------ ------------"
+};
+
+int main()
+{
+	return 0;
+}
